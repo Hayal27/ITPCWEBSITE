2e990b3cfe8a3d2a5b3b3edcc8d0e29f238c9c7c
yusufyusie <yesuf023@gmail.com>
2025-04-26 19:19:15 +0300

    add some style contaner

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/layout/Header/Header';
-import Footer from './components/Footer';
 import Home from './pages/Home/Home';
 import Incubation from './pages/Incubation/Incubation';
 import StartupPage from './pages/Startup/Startup';
 import StartupDirectory from './pages/StartupDirectory/StartupDirectory';
 import TrainingWorkshops from './pages/TrainingWorkshops/TrainingWorkshops';
+import MainLayout from './components/layout/MainLayout';
 
 const App: React.FC = () => (
   <Router>
-    <div className="app">
-      <Header />
+    <MainLayout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/incubation" element={<Incubation />} />
         <Route path="/incubation/startups" element={<StartupPage />} />
         <Route path="/incubation/startups/directory" element={<StartupDirectory />} />
         <Route path="/incubation/training" element={<TrainingWorkshops />} />
-          {/* Add more routes here as needed */}
-          </Routes>
-      <Footer />
-    </div>
+        {/* Add more routes here as needed */}
+      </Routes>
+    </MainLayout>
   </Router>
 );
 
