c146345065e690cd53cd8ec447f9d4c78ed49d5a
yusufyusie <yesuf023@gmail.com>
2025-05-07 12:47:52 +0300

    add mainlayout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react';
 import Header from './Header/Header';
 import Footer from '../Footer';
-import LiveChatWidget from '../LiveChatWidget';
 
 interface MainLayoutProps {
   children: ReactNode;
